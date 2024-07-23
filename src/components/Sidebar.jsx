897b6fee8f0a9dd33e1120e3bde88397c20ca7d7
Loc Tong <loc.tong@take1.vn>
2024-07-23 22:59:26 +0700

    add mini profile to the sidebar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,38 +1,55 @@
+/* eslint-disable @next/next/no-img-element */
 "use client";
 import Link from "next/link";
 import { FaXTwitter } from "react-icons/fa6";
-import { HiHome } from "react-icons/hi";
+import { HiHome, HiDotsHorizontal } from "react-icons/hi";
 import React from "react";
 import { signIn, signOut, useSession } from "next-auth/react";
 
 export default function Sidebar() {
   const { data: session } = useSession();
   return (
-    <div className="flex flex-col gap-4 p-3">
-      <Link href="/">
-        <FaXTwitter className="w-16 h-16 cursor-pointer p-3 hover:bg-gray-100 rounded-full transition-all duration-200" />
-      </Link>
-      <Link
-        href="/"
-        className="flex items-center p-3 hover:bg-gray-100 rounded-full transition-all duration-200 gap-2 w-fit"
-      >
-        <HiHome className="w-7 h-7 " />{" "}
-        <span className="font-bold hidden xl:inline">Home</span>
-      </Link>
-      {session ? (
-        <button
-          onClick={() => signOut()}
-          className="font-semibold bg-blue-400 text-white rounded-full hover:brightness-95 transition-all duration-200 w-48 h-9 shadow-md hidden xl:inline"
+    <div className="flex flex-col p-3 justify-between h-screen">
+      <div className="flex flex-col gap-4">
+        <Link href="/">
+          <FaXTwitter className="w-16 h-16 cursor-pointer p-3 hover:bg-gray-100 rounded-full transition-all duration-200" />
+        </Link>
+        <Link
+          href="/"
+          className="flex items-center p-3 hover:bg-gray-100 rounded-full transition-all duration-200 gap-2 w-fit"
         >
-          Sign Out
-        </button>
-      ) : (
-        <button
-          onClick={() => signIn()}
-          className="font-semibold bg-blue-400 text-white rounded-full hover:brightness-95 transition-all duration-200 w-48 h-9 shadow-md hidden xl:inline"
-        >
-          Sign In
-        </button>
+          <HiHome className="w-7 h-7 " />{" "}
+          <span className="font-bold hidden xl:inline">Home</span>
+        </Link>
+        {session ? (
+          <button
+            onClick={() => signOut()}
+            className="font-semibold bg-blue-400 text-white rounded-full hover:brightness-95 transition-all duration-200 w-48 h-9 shadow-md hidden xl:inline"
+          >
+            Sign Out
+          </button>
+        ) : (
+          <button
+            onClick={() => signIn()}
+            className="font-semibold bg-blue-400 text-white rounded-full hover:brightness-95 transition-all duration-200 w-48 h-9 shadow-md hidden xl:inline"
+          >
+            Sign In
+          </button>
+        )}
+      </div>
+      {session && (
+        <div className="text-gray-700 text-sm flex items-center cursor-pointer p-3 hover:bg-gray-100 rounded-full transition-all duration-200">
+          <img
+            src={session.user.image}
+            alt="user-img"
+            className="h-10 rounded-full xl:mr-2"
+          />
+          <div className="hidden xl:inline">
+            <h4 className="font-bold">{session.user.name}</h4>
+            <p className="text-gray-500">@{session.user.username}</p>
+          </div>
+          <HiDotsHorizontal className="h-5 xl:ml-8 hidden xl:inline" />
+        </div>
       )}
     </div>
   );
